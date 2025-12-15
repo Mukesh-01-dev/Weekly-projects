2c97e65d2a89f0541fbda874c137508ff521440e
Mukesh Choudhary <mukeshchoudhary33333@gmail.com>
2025-12-15 20:35:28 +0530

    new practise programs

---
+++ C_250+_Programs/1_Simple_Programs/AreaCircumferece.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+  float pi = 3.14;
+  int radius;
+
+  printf("Enter the radius of the circle: ");
+  scanf("%d", &radius);
+
+  if (scanf("%d", &radius) != 1) {
+    printf("Invalid input\n");
+    return 1;
+  }
+
+  float area = pi * (radius * radius);
+  float circumference = 2 * pi * radius;
+
+  printf("The circumference of the circle is: %f\n", circumference);
+  printf("The area of the circle is: %f", area);
+
+  return 0;
+}
